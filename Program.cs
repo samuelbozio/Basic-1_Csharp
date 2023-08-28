632a8cf05842e52490fa71e422c61359adb95f5d
Samuel Bozio <samuelbozio@hotmail.com>
2023-08-28 16:06:00 -0300

    primeiros contatos c#

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace PrimeirProjeto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Digite quantos alunos quer cadastrar: ");
+            int quantAlunos = Int32.Parse(Console.ReadLine());
+            string[] nomes = new string[quantAlunos];
+
+
+           for (int i = 1; i <= nomes.Length; i++)
+            {
+                Console.WriteLine("Nome do aluno: " + i);
+                nomes[i] = Console.ReadLine();
+            }
+
+
+
+
+        }
+
+
+    }
+
+
+}
+
+
